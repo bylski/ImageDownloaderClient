db055b9de540d421fd2728e4bdc2e12c617fd8e2
Patryk Przybylski <bylski.patryk@gmail.com>
2023-03-26 04:16:49 +0200

    Finish base carousel implementation

--- src/components/Queue/Queue.tsx
+++ src/components/Queue/Queue.tsx
@@ -1,21 +1,30 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, Fragment } from "react";
 import styles from "./styles/Queue.module.scss";
 import QueueElement from "./QueueElement";
 import { AppContext } from "../../store/app-context";
+import { AnimatePresence, LayoutGroup } from "framer-motion";
 
 const Queue: React.FC = () => {
   const ctx = useContext(AppContext);
- 
 
-  const queueElements = ctx?.queueElements.map((item) => {
-    return <QueueElement title={item.title} />;
+  const queueElements = ctx?.queueElements.map((item, i) => {
+    return (
+      <AnimatePresence>
+        <QueueElement key={`item${i}`} title={item.title} />
+      </AnimatePresence>
+    );
   });
-
 
   return (
     <section className={styles["queue"]}>
       <span className={styles["label"]}>Upload Queue:</span>
-      <div className={styles["track"]}>{queueElements}</div>
+      <div className={styles["track"]}>
+        <AnimatePresence>
+          {ctx?.queueElements.length! < 3 && <QueueElement title={"das"} />}
+        </AnimatePresence>
+
+        {queueElements}
+      </div>
     </section>
   );
 };
--- src/components/Queue/QueueElement.tsx
+++ src/components/Queue/QueueElement.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import styles from "./styles/QueueElement.module.scss";
-import { isPropertySignature } from "typescript";
 import { limitStr } from "../../utils/helper-functions/limitStr";
+import { motion, AnimatePresence } from "framer-motion";
 
 const QueueElement: React.FC<{ title: string }> = (props) => {
   return (
-    <div className={styles["queue-element"]}>
-      <span className={styles["title"]}>{limitStr(props.title, 60)}</span>
-    </div>
+      <motion.div
+        transition={{ type: "spring", damping: 100, stiffness: 800 }}
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 1 }}
+        className={styles["queue-element"]}
+        
+      >
+        <span className={styles["title"]}>{limitStr(props.title, 60)}</span>
+        <div className={styles["loading-circle"]}></div>
+      </motion.div>
   );
 };
 
